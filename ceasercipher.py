4f72d0efd05a125d9dd45328605335db68f9863a
a28k12 <ak5469553@gmail.com>
2021-01-03 18:49:57 +0530

    this is ceaser cipher commit

---
+++ ceasercipher.py
@@ -0,0 +1,31 @@
+def encrypt(text,s):
+  result=""
+  for i in range(len(text)):
+    char=text[i]
+    if(char.isupper()):
+      result += chr((ord(char)+ s - 65) % 26 + 65)
+    else:
+      result += chr((ord(char) + s - 97) % 26 + 97)
+  return result
+
+def decrypt(text,s):
+  result=""
+  for i in range(len(text)):
+    char=text[i]
+    if(char.isupper()):
+      result += chr((ord(char)- s - 65) % 26 + 65)
+    else:
+      result += chr((ord(char) - s - 97) % 26 + 97)
+  return result
+
+text=input("Enter the  text ")
+shift=int(input("Enter the shift No "))
+print("ENCRYPTION..............")
+print("Plain text: "+ text)
+print("Shift no :"+ str(shift))
+cipher=encrypt(text,shift)
+print("Cipher Text: "+ cipher )
+print("DECRYPTION.........")
+print("Cipher Text : "+ cipher)
+print("Original Text : "+ decrypt(cipher,shift))
+
